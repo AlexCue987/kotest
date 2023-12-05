ff6cca6269ab15fcee9a963ba847b4e8387b3467
AlexKuznetsov <alexander.kuznetsov@target.com>
2023-12-05 14:40:22 -0600

    test-for-shouldNot

--- kotest-assertions/kotest-assertions-core/src/jvmTest/kotlin/com/sksamuel/kotest/matchers/date/LocalTimeToleranceMatcherTest.kt
+++ kotest-assertions/kotest-assertions-core/src/jvmTest/kotlin/com/sksamuel/kotest/matchers/date/LocalTimeToleranceMatcherTest.kt
@@ -7,6 +7,7 @@ import io.kotest.matchers.date.LocalTimeToleranceMatcher
 import io.kotest.matchers.date.and
 import io.kotest.matchers.date.plusOrMinus
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 import java.time.LocalTime
 import kotlin.time.Duration.Companion.hours
 import kotlin.time.Duration.Companion.minutes
@@ -46,6 +47,41 @@ class LocalTimeToleranceMatcherTest: WordSpec() {
             shouldThrowAny {
                LocalTime.of(0, 4) shouldBe (oneMinuteAfterMidnight plusOrMinus 2.minutes)
             }.message shouldBe "00:04 should be equal to 00:01 with tolerance 2m (between 23:59 and 00:03 next day)"
+         }
+      }
+
+      "shouldNot" should {
+         "fail when the whole range is inside one calendar day" {
+            shouldThrowAny {
+               onePm shouldNotBe (LocalTime.of(13, 1, 0) plusOrMinus 2.minutes)
+            }.message shouldBe "13:00 should not be equal to 13:01 with tolerance 2m (not between 12:59 and 13:03)"
+            shouldThrowAny {
+               onePm shouldNotBe (LocalTime.of(12, 59, 0) plusOrMinus 2.minutes)
+            }.message shouldBe "13:00 should not be equal to 12:59 with tolerance 2m (not between 12:57 and 13:01)"
+         }
+
+         "pass when the whole range is inside one calendar day" {
+            onePm shouldNotBe (LocalTime.of(13, 3, 0) plusOrMinus 2.minutes)
+            onePm shouldNotBe (LocalTime.of(12, 57, 0) plusOrMinus 2.minutes)
+         }
+
+         "fail when the range includes midnight" {
+            val oneMinuteAfterMidnight = LocalTime.of(0, 1, 0)
+            shouldThrowAny {
+               LocalTime.of(23, 59) shouldNotBe (oneMinuteAfterMidnight plusOrMinus 2.minutes)
+            }.message shouldBe "23:59 should not be equal to 00:01 with tolerance 2m (not between 23:59 and 00:03 next day)"
+            shouldThrowAny {
+               LocalTime.of(0, 0) shouldNotBe (oneMinuteAfterMidnight plusOrMinus 2.minutes)
+            }.message shouldBe "00:00 should not be equal to 00:01 with tolerance 2m (not between 23:59 and 00:03 next day)"
+            shouldThrowAny {
+               LocalTime.of(0, 2) shouldNotBe (oneMinuteAfterMidnight plusOrMinus 2.minutes)
+            }.message shouldBe "00:02 should not be equal to 00:01 with tolerance 2m (not between 23:59 and 00:03 next day)"
+         }
+
+         "pass when the range includes midnight" {
+            val oneMinuteAfterMidnight = LocalTime.of(0, 1, 0)
+            LocalTime.of(23, 58) shouldNotBe (oneMinuteAfterMidnight plusOrMinus 2.minutes)
+            LocalTime.of(0, 4) shouldNotBe (oneMinuteAfterMidnight plusOrMinus 2.minutes)
          }
       }
 
