69f923f8cc8d4b577866022c8f3b08e8725960be
AlexKuznetsov <alexander.kuznetsov@target.com>
2024-01-23 10:46:44 -0600

    upper

---
+++ kotest-assertions/kotest-assertions-core/src/jvmTest/kotlin/com/sksamuel/kotest/matchers/collections/BoundsTest.kt
@@ -0,0 +1,21 @@
+package com.sksamuel.kotest.matchers.collections
+
+import io.kotest.assertions.throwables.shouldThrowAny
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.collections.shouldHaveUpperBound
+import io.kotest.matchers.throwable.shouldHaveMessage
+
+class BoundsTest: WordSpec() {
+   init {
+      "haveUpperBound" should {
+         "pass" {
+            listOf(1, 2, 3) shouldHaveUpperBound 3
+         }
+         "fail" {
+            shouldThrowAny {
+               listOf(1, 2, 3) shouldHaveUpperBound 2
+            }.shouldHaveMessage("Collection should have upper bound 2, but the following elements are above it: [3]")
+         }
+      }
+   }
+}
