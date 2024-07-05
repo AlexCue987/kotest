34f5993191355eaa541156719172f79a4ec27d1a
AlexKuznetsov <alexander.kuznetsov@target.com>
2024-07-05 13:23:22 -0500

    Submatching.kt

---
+++ kotest-assertions/kotest-assertions-core/src/commonMain/kotlin/io/kotest/submatching/Submatching.kt
@@ -0,0 +1,50 @@
+package io.kotest.submatching
+
+
+internal fun toCharIndex(value: String): Map<Char, List<Int>> {
+   return value.mapIndexed { index, char ->
+      index to char
+   }.groupBy(keySelector = { it.second }, valueTransform = { it.first })
+}
+
+internal fun findPartialMatches(value: String, target: String, minLength: Int): List<PartialMatch> {
+    val indexes = toCharIndex(target)
+    return value.asSequence().mapIndexed { index, char ->
+        index to char
+    }.filter { pair -> pair.first + minLength <= value.length }
+       .flatMap { pair -> indexes[pair.second]?.map { index -> MatchedCharacter(
+            indexInValue = pair.first,
+            indexInTarget = index
+        )
+       } ?: listOf()
+       }.mapNotNull { matchedCharacter ->
+          val lengthOfMatch = lengthOfMatch(value, target, matchedCharacter)
+          if(lengthOfMatch >= minLength) {
+             PartialMatch(
+                matchedCharacter,
+                lengthOfMatch,
+                value.substring(matchedCharacter.indexInValue, matchedCharacter.indexInValue + lengthOfMatch)
+             )
+          } else null
+       }.toList()
+}
+
+internal fun lengthOfMatch(
+   value: String, target: String, matchedCharacter: MatchedCharacter
+): Int {
+   val maxLengthOfMatch = minOf(value.length - matchedCharacter.indexInValue, target.length - matchedCharacter.indexInTarget)
+   return (1..maxLengthOfMatch).takeWhile { offset ->
+      value[matchedCharacter.indexInValue + offset - 1] == target[matchedCharacter.indexInTarget + offset - 1]
+   }.lastOrNull() ?: 0
+}
+
+internal data class MatchedCharacter(
+   val indexInValue: Int,
+   val indexInTarget: Int
+)
+
+internal data class PartialMatch(
+   val matchedCharacter: MatchedCharacter,
+   val length: Int,
+   val partOfValue: String
+)
---
+++ kotest-assertions/kotest-assertions-core/src/jvmTest/kotlin/io/kotest/submatching/SubmatchingTest.kt
@@ -0,0 +1,88 @@
+package io.kotest.submatching
+
+import io.kotest.assertions.withClue
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.collections.shouldBeEmpty
+import io.kotest.matchers.shouldBe
+
+class SubmatchingTest: WordSpec() {
+   init {
+      "findPartialMatches" should {
+         "find nothing" {
+            findPartialMatches("apple", "orange", minLength = 3).shouldBeEmpty()
+         }
+         "find common substring" {
+            findPartialMatches("broom", "roommate", minLength = 4) shouldBe listOf(
+               PartialMatch(MatchedCharacter(1, 0), 4, "room"))
+         }
+         "find common end" {
+            findPartialMatches("river", "driver", minLength = 5) shouldBe listOf(
+               PartialMatch(MatchedCharacter(0, 1), 5, "river"))
+         }
+         "find two common substrings in same order" {
+            findPartialMatches("roommate", "room-mate", minLength = 4) shouldBe listOf(
+               PartialMatch(MatchedCharacter(0, 0), 4, "room"),
+               PartialMatch(MatchedCharacter(4, 5), 4, "mate"),
+            )
+         }
+      }
+      "toCharIndex" should {
+         "count" {
+            toCharIndex("apple") shouldBe mapOf(
+               'a' to listOf(0),
+               'p' to listOf(1, 2),
+               'l' to listOf(3),
+               'e' to listOf(4)
+            )
+         }
+      }
+      "lengthOfMatch" should {
+         "return 0 at mismatch" {
+            (0..3).forEach { start ->
+               withClue("Matching target at index $start") {
+                  lengthOfMatch(
+                     value = "bug",
+                     target = "feature",
+                     matchedCharacter = MatchedCharacter(0, start)
+                  ) shouldBe 0
+               }
+            }
+         }
+         "mismatch on first char" {
+            lengthOfMatch(
+               value = "prone",
+               target = "drone",
+               matchedCharacter = MatchedCharacter(0, 0)
+            ) shouldBe 0
+         }
+         "skip some chars at start" {
+            lengthOfMatch(
+               value = "prone",
+               target = "drone",
+               matchedCharacter = MatchedCharacter(1, 1)
+            ) shouldBe 4
+         }
+         "find common start" {
+            lengthOfMatch(
+               value = "car",
+               target = "cartoon",
+               matchedCharacter = MatchedCharacter(0, 0)
+            ) shouldBe 3
+         }
+         "stop at shorter substring in the middle of a loger one" {
+            lengthOfMatch(
+               value = "rip",
+               target = "tripod",
+               matchedCharacter = MatchedCharacter(0, 1)
+            ) shouldBe 3
+         }
+         "find common end" {
+            lengthOfMatch(
+               value = "come",
+               target = "outcome",
+               matchedCharacter = MatchedCharacter(0, 3)
+            ) shouldBe 4
+         }
+      }
+   }
+}
